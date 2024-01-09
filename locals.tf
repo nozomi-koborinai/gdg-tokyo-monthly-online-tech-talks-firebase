e998f157cd146691a515008bad519e3139ef7e3c
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-01-09 23:39:33 +0900

    docs: add firesotre documents

--- locals.tf
+++ locals.tf
@@ -12,5 +12,29 @@ locals {
         }
       })
     },
+    "question-2" = {
+      collection  = "quiz"
+      document_id = "question-2"
+      fields = jsonencode({
+        "question" = {
+          "stringValue" = "Second Question"
+        },
+        "answer" = {
+          "stringValue" = "Answer 2"
+        }
+      })
+    },
+    "question-3" = {
+      collection  = "quiz"
+      document_id = "question-3"
+      fields = jsonencode({
+        "question" = {
+          "stringValue" = "Third Question"
+        },
+        "answer" = {
+          "stringValue" = "Answer 3"
+        }
+      })
+    },
   }
 }
