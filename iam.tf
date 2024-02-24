2ee3802a2b81fe1da8cd3ef2588a867247b6385c
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-02-24 14:19:08 +0900

    fix: resource name

--- iam.tf
+++ iam.tf
@@ -8,5 +8,5 @@ resource "google_project_iam_member" "demo_iam_member" {
   for_each = local.accounts
   project  = var.project_id
   role     = each.value.role
-  member   = "serviceAccount:${google_service_account.default[each.key].email}"
+  member   = "serviceAccount:${google_service_account.demo_service_account[each.key].email}"
 }
