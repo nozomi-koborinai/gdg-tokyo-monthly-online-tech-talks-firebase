85210991f44ab644a24008b52309733eb2aa3315
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-02-24 13:15:47 +0900

    style: terraform fmt

--- iam.tf
+++ iam.tf
@@ -3,25 +3,25 @@ locals {
     {
       account_id   = "User01"
       display_name = "デモ用のサービスアカウント①"
-      role        = "roles/owner"
+      role         = "roles/owner"
     },
     {
       account_id   = "User02"
       display_name = "デモ用のサービスアカウント②"
-      role = "roles/editor"
+      role         = "roles/editor"
     },
   ]
 }
 
 resource "google_service_account" "default" {
-    for_each = { for account in locals.accounts : account.account_id => account }
-    account_id   = each.value.account_id
-    display_name = each.value.display_name
+  for_each     = { for account in locals.accounts : account.account_id => account }
+  account_id   = each.value.account_id
+  display_name = each.value.display_name
 }
 
 resource "google_project_iam_member" "default" {
-    for_each = { for account in locals.accounts : account.account_id => account }
-    project = var.project_id
-    role    = each.value.role
-    member  = "serviceAccount:${google_service_account.default[each.key].email}"
+  for_each = { for account in locals.accounts : account.account_id => account }
+  project  = var.project_id
+  role     = each.value.role
+  member   = "serviceAccount:${google_service_account.default[each.key].email}"
 }
