a523e8b4b8a830a2dd3bb7c546dbda0d0ccfe2a8
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-02-24 13:14:44 +0900

    add: iam_member for demo

---
+++ iam.tf
@@ -0,0 +1,27 @@
+locals {
+  accounts = [
+    {
+      account_id   = "User01"
+      display_name = "デモ用のサービスアカウント①"
+      role        = "roles/owner"
+    },
+    {
+      account_id   = "User02"
+      display_name = "デモ用のサービスアカウント②"
+      role = "roles/editor"
+    },
+  ]
+}
+
+resource "google_service_account" "default" {
+    for_each = { for account in locals.accounts : account.account_id => account }
+    account_id   = each.value.account_id
+    display_name = each.value.display_name
+}
+
+resource "google_project_iam_member" "default" {
+    for_each = { for account in locals.accounts : account.account_id => account }
+    project = var.project_id
+    role    = each.value.role
+    member  = "serviceAccount:${google_service_account.default[each.key].email}"
+}
